730b0a3a0ba2ed307a5f67b54c8117aeca4441a9
Kenia Montalvo <kenlimonra@gmail.com>
2025-05-01 01:37:53 -0600

    se agrega formato de fecha

--- script/script.js
+++ script/script.js
@@ -8,7 +8,25 @@ document.addEventListener('DOMContentLoaded', function() {
     
     function generarMinuta() {
         // Obtener valores del formulario
+        // Obtener la fecha seleccionada
+        const fechaInput = document.getElementById('fechaMinuta').value;
+        const fecha = new Date(fechaInput);
+
+        // Formatear la fecha a un formato (ej: "01 de mayo de 2025")
+        const opcionesFecha = { 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric' 
+        };
+
+        const fechaFormateada = fecha.toLocaleDateString('es-ES', opcionesFecha);
+
+        
         const tituloValor = document.getElementById('tituloValor').value;
+        document.getElementById('minutaTitulo').innerHTML = `
+        Minuta Semanal - SI ${tituloValor}`;
+        document.getElementById('minutaFecha').innerHTML = `
+        Fecha: ${fechaFormateada}`;
         const numeroProyecto = document.getElementById('numeroProyecto').value;
         const nombreProyecto = document.getElementById('nombreProyecto').value;
         const descripcion = document.getElementById('descripcion').value;
@@ -29,6 +47,19 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('minutaSolicitante').textContent = solicitante;
         document.getElementById('minutaAplicaRapid7').textContent = aplicaRapid7;
         document.getElementById('minutaAplicaPrisma').textContent = aplicaPrisma;
+
+    // Colores para las respuestas de checklist
+    // Rapid7
+    const rapid7Element = document.getElementById('minutaAplicaRapid7');
+    rapid7Element.textContent = aplicaRapid7;
+    rapid7Element.className = 'infoPreguntas ' + (aplicaRapid7 === 'Sí aplica' ? 'respuesta-si' : 'respuesta-no');
+    
+    // Prisma
+    const prismaElement = document.getElementById('minutaAplicaPrisma');
+    prismaElement.textContent = aplicaPrisma;
+    prismaElement.className = 'infoPreguntas ' + (aplicaPrisma === 'Sí aplica' ? 'respuesta-si' : 'respuesta-no');
+
+
         document.getElementById('minutaObservaciones').textContent = observaciones;
         
         // Mostrar la minuta generada
